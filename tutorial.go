c4fb8748b93e0fe5e18840a0d170645079ce0acc
gabetucker2 <gabe@the-tuckers.com>
2022-08-17 14:23:19 -0400

    New

--- tutorial.go
+++ tutorial.go
@@ -1,31 +1,48 @@
 package sample
 
 import (
-	. "github.com/gabetucker2/govariadics"
+	"github.com/gabetucker2/govariadics"
 )
 
 /**
  The following is the recommended template for how you should structure functions using this library:
 */
-func sampleFunction(variadic ...any) {
+func myFunction(variadic ...any) {
 
 	// unpack variadic into optional parameters
 	var name, age, height any
-	UnpackVariadic(variadic, &name, &age, &height)
+	govariadics.UnpackVariadic(variadic, &name, &age, &height)
 
 	// sample body stuff below:
-	if name == "Gerry" {
-		// do stuff
+	if name == nil {
+		// no name was entered
 	}
 	if age == 34 {
-		// do stuff
+		// age 34 was entered
 	}
-	if height == 2.6 {
-		// do stuff
+	if height == nil {
+		// no height was entered
 	}
 
 }
 
-func main() {
-	sampleFunction("Gerry", 20, 5.9)
+func sample() {
+
+	// call A
+	myFunction("Gerry", 20, 5.9)
+
+	// call B (all are the same)
+	myFunction("Gerry", 20)
+	myFunction("Gerry", 20, nil)
+
+	// call C (all are the same)
+	myFunction(nil, 20)
+	myFunction(nil, 20, nil)
+
+	// call D (all are the same)
+	myFunction()
+	myFunction(nil)
+	myFunction(nil, nil)
+	myFunction(nil, nil, nil)
+
 }
